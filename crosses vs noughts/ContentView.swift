85d5c06d0b460066ea667439256bbdc355a82ae9
Tesloboy <meex_v92@mail.ru>
2024-04-10 14:42:39 +0300

    Initial Commit

---
+++ crosses vs noughts/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  crosses vs noughts
+//
+//  Created by Viktor Teslenko on 10.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ crosses vs noughts/crosses_vs_noughtsApp.swift
@@ -0,0 +1,17 @@
+//
+//  crosses_vs_noughtsApp.swift
+//  crosses vs noughts
+//
+//  Created by Viktor Teslenko on 10.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct crosses_vs_noughtsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
