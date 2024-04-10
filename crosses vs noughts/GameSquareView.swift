60de10f438e232024fcddf0f99ea71b442b32ff3
Tesloboy <meex_v92@mail.ru>
2024-04-10 15:15:24 +0300

    Added a game screen and two modes: playing against the computer and playing with a human.

---
+++ crosses vs noughts/GameSquareView.swift
@@ -0,0 +1,8 @@
+//
+//  11.swift
+//  Crosses vs Noughts
+//
+//  Created by Viktor Teslenko on 10.04.2024.
+//
+
+import Foundation
---
+++ crosses vs noughts/GameViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  GameViewModel.swift
+//  Crosses vs Noughts
+//
+//  Created by Viktor Teslenko on 10.04.2024.
+//
+
+import Foundation
